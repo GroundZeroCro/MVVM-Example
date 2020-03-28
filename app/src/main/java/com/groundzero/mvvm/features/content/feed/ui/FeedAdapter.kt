977ae0e59ac11a7877d1fe18b9ac43240405dbcf
Mario Dujić <liberasoftware@gmail.com>
2020-03-28 14:15:51 +0100

    added feed cards

---
+++ app/src/main/java/com/groundzero/mvvm/features/content/feed/ui/FeedAdapter.kt
@@ -0,0 +1,37 @@
+package com.groundzero.mvvm.features.content.feed.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.groundzero.mvvm.databinding.ItemFeedBinding
+import com.groundzero.mvvm.features.content.feed.domain.Feed
+
+class FeedAdapter : ListAdapter<Feed, FeedAdapter.FeedViewHolder>(DIFF_CALLBACK) {
+
+    class FeedViewHolder(private val binding: ItemFeedBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(feed: Feed) {
+            binding.feed = feed
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedViewHolder =
+        FeedViewHolder(ItemFeedBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onBindViewHolder(holder: FeedViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Feed>() {
+            override fun areItemsTheSame(oldItem: Feed, newItem: Feed): Boolean =
+                oldItem.id == newItem.id
+
+            override fun areContentsTheSame(oldItem: Feed, newItem: Feed): Boolean =
+                oldItem == newItem
+        }
+    }
+}
--- app/src/main/java/com/groundzero/mvvm/features/content/feed/ui/FeedFragment.kt
+++ app/src/main/java/com/groundzero/mvvm/features/content/feed/ui/FeedFragment.kt
@@ -5,21 +5,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
+import com.groundzero.mvvm.R
 import com.groundzero.mvvm.base.BaseFragment
+import com.groundzero.mvvm.data.Result
 import com.groundzero.mvvm.databinding.FragmentFeedBinding
 import com.groundzero.mvvm.di.helper.injectViewModel
 
 class FeedFragment : BaseFragment() {
 
     private lateinit var viewModel: FeedViewModel
+    private var feedAdapter: FeedAdapter = FeedAdapter()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? = FragmentFeedBinding.inflate(inflater, container, false).apply {
+        feedRecyclerView.adapter = feedAdapter
         viewModel = injectViewModel(viewModelFactory)
         viewModel.feed.observe(viewLifecycleOwner, Observer {
-            println(it)
+
+            when (it.status) {
+                Result.Status.LOADING ->
+                    progressDialog.showDialog(
+                        requireContext(),
+                        requireContext().getString(R.string.progress_bar_loading_feed)
+                    )
+                Result.Status.SUCCESS -> {
+                    progressDialog.cancelDialog()
+                    feedAdapter.submitList(it.data)
+                }
+                Result.Status.ERROR -> {
+                }
+            }
         })
     }.root
 }
