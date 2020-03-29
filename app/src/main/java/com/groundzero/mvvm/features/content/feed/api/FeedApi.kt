61daf933a3b7d9bb5c5c6d47c6c27d9f1bb50735
Mario Dujić <liberasoftware@gmail.com>
2020-03-29 15:10:00 +0200

    added TODO comment

--- app/src/main/java/com/groundzero/mvvm/features/content/feed/api/FeedApi.kt
+++ app/src/main/java/com/groundzero/mvvm/features/content/feed/api/FeedApi.kt
@@ -5,6 +5,7 @@ import retrofit2.http.GET
 
 interface FeedApi {
 
+    // TODO implement pagination
     @GET("feed?page=1&sport=football")
     suspend fun getFeed(): Response<List<FeedResponse>>
 }
